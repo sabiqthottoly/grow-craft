9fe8702433b883e102eda2cf18b30171a14a436d
sabiqthottoly <sabiqt@gmail.com>
2021-12-20 01:47:58 +0530

    Create slider

--- Js/Index.js
+++ Js/Index.js
@@ -1,3 +1,12 @@
+var counter = 1;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true
+    counter++
+    if (counter > 4) {
+        counter = 1
+    }
+}, 5000)
+
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
